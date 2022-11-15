0a7e7e64afd6263b5a63a1e55156bba08616a677
Marsel Yumagulov <marseliumagulov@yandex.ru>
2022-11-15 21:54:47 +0300

    4.6.2. Switch - matcher [#334071]

--- src/main/java/ru/job4j/condition/MultipleSwitchWeek.java
+++ src/main/java/ru/job4j/condition/MultipleSwitchWeek.java
@@ -15,6 +15,6 @@ public class MultipleSwitchWeek {
     }
 
     public static void main(String[] args) {
-        System.out.println(numberOfDay("Sunday"));
+        System.out.println(numberOfDay("‡‡‡"));
     }
 }
