04762bb8184dc0aa2aeb7907df19ade98b32d372
Marsel Yumagulov <marseliumagulov@yandex.ru>
2022-11-20 16:37:08 +0300

    6.0.3. Заполнение массива. [#226300]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -8,5 +8,14 @@ public class ArrayDefinition {
         System.out.println("Размер массива равен: " + surenames.length);
         float[] prices = new float[40];
         System.out.println("Размер массива равен: " + prices.length);
+        String[] names = new String[4];
+        names[0] = "Petr Yan";
+        names[1] = "Fedor Emelianenko";
+        names[2] = "Connor McGregor";
+        names[3] = "Charles Oliveira";
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
     }
 }
